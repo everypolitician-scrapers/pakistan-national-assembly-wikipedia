62d4ac58c9113ad39fc2edc6b6af36cf26e157dc
Chris Mytton <chrismytton@gmail.com>
2017-09-19 16:11:37 +0100

    Scrape membership changes table as well
    This is a list of all the people that were a member of the 14th term but
    for whatever reason no longer are.

--- scraper.rb
+++ scraper.rb
@@ -19,6 +19,10 @@ class MemberList < Scraped::HTML
     member_rows.map { |tr| fragment(tr => MemberRow).to_h }
   end
 
+  def areas
+    @areas ||= members.reject { |m| m[:area_id].nil? }.map { |m| [ m[:area], m[:area_id] ] }.to_h
+  end
+
   def parties
     @parties ||= members.reject { |m| m[:party_id].nil? }.map { |m| [ m[:party], m[:party_id] ] }.to_h
   end
@@ -87,12 +91,81 @@ class MemberRow < Scraped::HTML
   end
 end
 
+class MembershipChanges < Scraped::HTML
+  decorator WikidataIdsDecorator::Links
+
+  field :members do
+    member_rows.map { |tr| fragment(tr => MembershipChangeRow).to_h }
+  end
+
+  private
+
+  def membership_changes_table
+    noko.xpath('//h2[span[@id="Membership_changes"]]/following-sibling::table[1]')
+  end
+
+  def member_rows
+    # all rows with a link in the 3rd column
+    membership_changes_table.xpath('.//tr[td[3][a]]')
+  end
+end
+
+class MembershipChangeRow < Scraped::HTML
+  field :region do
+    noko.at_css('th').text.tidy
+  end
+
+  field :party do
+    td[1].text.tidy
+  end
+
+  field :name do
+    member.text.tidy
+  end
+
+  field :id do
+    member.attr('wikidata') rescue binding.pry
+  end
+
+  field :area_id do
+    constituency_link && constituency_link.attr('wikidata')
+  end
+
+  field :area do
+    constituency.text.tidy
+  end
+
+  private
+
+  def td
+    noko.css('td')
+  end
+
+  def member
+    td[2].at_css('a')
+  end
+
+  def constituency_link
+    td[0].css('a').first
+  end
+
+  def constituency
+    td[0]
+  end
+end
+
 url = 'https://en.wikipedia.org/wiki/List_of_members_of_the_14th_National_Assembly_of_Pakistan'
 
-page = scraped(url => MemberList)
-data = page.members.each do |mem|
-  mem[:party_id] ||= page.parties[mem[:party]]
+member_list = scraped(url => MemberList)
+membership_changes = scraped(url => MembershipChanges)
+
+data = member_list.members + membership_changes.members
+
+data.each do |mem|
+  mem[:party_id] ||= member_list.parties[mem[:party]]
+  mem[:area_id] ||= member_list.areas[mem[:area]]
 end
+
 data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
